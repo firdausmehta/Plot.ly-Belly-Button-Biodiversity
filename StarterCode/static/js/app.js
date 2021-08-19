5f0dca244550b98d1529d231d3b4894b90c328b1
Firdaus Mehta <firmehta97@gmail.com>
2021-08-19 19:51:18 +1000

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,3 +23,32 @@ function startup() {
 
     }).catch(error => console.log(error));
 };
+
+// Dynamic changing of plots and demographics upon change in dropdown
+function optionChanged(newID){
+    buildPlots(newID);
+    demographics(newID);
+};
+
+// Building Bar Chart and Bubble Chart
+function buildPlots(id) {
+    // Reading in the json dataset
+    d3.json("samples.json").then(function(samplesData){
+        // console.log(samplesData);
+        // Filtering for the id selected
+        var filtered = samplesData.samples.filter(sample => sample.id == id);
+        var result = filtered[0];
+        // console.log(filtered)
+        // console.log(result)
+
+        // creating variables and storing the top 10 in an array
+
+        Data = [];
+        for (i=0; i<result.sample_values.length; i++){
+            Data.push({
+                id: `OTU ${result.otu_ids[i]}`,
+                value: result.sample_values[i],
+                label: result.otu_labels[i]
+            });
+        }
+        // console.log(Data);
