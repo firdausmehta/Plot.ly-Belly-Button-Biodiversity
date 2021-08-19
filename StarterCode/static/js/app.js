e739c994e168b2491235bf969afd4b7037f4ac88
Firdaus Mehta <firmehta97@gmail.com>
2021-08-19 19:00:49 +1000

    Update app.js

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,25 @@
+// Initializing the page and calling the other functions
+function startup() {
+
+    // Grabbing the dropdown element
+    var selector = d3.select('#selDataset');
+
+    d3.json("samples.json").then(function(samplesData){
+        var names = samplesData.names;
+
+        selector.selectAll('option')
+            .data(names)
+            .enter()
+            .append('option')
+            .attr('value', d => d)
+            .text(d => d);
+
+        // Take in the first name upon loading the page
+        var starter = names[0];
+
+        // Call other functions using starter name
+        buildPlots(starter);
+        demographics(starter);
+
+    }).catch(error => console.log(error));
+};
